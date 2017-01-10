3452601ba6c13961e771cc566c5dd8ebbe9cacd5
Samarth Paboowal <samarthpaboowal@Samarths-MacBook-Air.local>
2017-01-10 12:55:47 +0530

    Added create and get commands to create and get accounts

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
-console.log('Starting Password Manager...');
+console.log('Starting Password Manager...\n');
 
 var storage = require('node-persist');
 storage.initSync();
+
+var argv = require('yargs')
+    .command('create', 'Create a new account', function (yargs) {
+      yargs.options({
+        name: {
+          demand: true,
+          alias: 'n',
+          description: 'Account name (eg: Twitter, Facebook)',
+          type: 'string'
+        },
+        username: {
+          demand: true,
+          alias: 'u',
+          description: 'Account username or email (eg: test123, test123@testmail.com)',
+          type: 'string'
+        },
+        password: {
+          demand: true,
+          alias: 'p',
+          description: 'Account password (eg: mypassword)',
+          type: 'string'
+        }
+      }).help('help');
+    })
+    .command('get', 'Get an existing account', function (yargs) {
+      yargs.options({
+        name: {
+          demand: true,
+          alias: 'n',
+          description: 'Account name (eg: Twitter, Facebook)',
+          type: 'string'
+        }
+      }).help('help');
+    })
+    .help('help')
+    .argv;
+
+var command = argv._[0];
 
 function createAccount (account) {
   var accounts = storage.getItemSync('accounts');
@@ -29,11 +67,22 @@ function getAccount (accountName) {
   return matchedAccount;
 }
 
-/*createAccount({
-  'name': 'Facebook',
-  'username': 'samarthpaboowal@yahoo.co.in',
-  'password': 'password'
-});
-*/
-
-//console.log(getAccount('Facebook'));
+if (command === 'create') {
+  var createdAccount = createAccount({
+    name: argv.name,
+    username: argv.username,
+    password: argv.password
+  });
+  console.log('Account Created!\n');
+  console.log(createdAccount);
+  console.log();
+} else if (command === 'get') {
+  var fetchedAccount = getAccount(argv.name);
+  if (typeof fetchedAccount === 'undefined') {
+    console.log('Account not found!\n');
+  } else {
+    console.log('Account Found\n');
+    console.log(fetchedAccount);
+    console.log();
+  }
+}
